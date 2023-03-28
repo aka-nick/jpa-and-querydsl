ecd8c60875f14b97b530bdb7cb465c0ff07c00e3
aka-nick <bechalrang@gmail.com>
2023-03-28 14:06:23 +0900

    init test

---
+++ src/main/java/study/querydsl/HelloContoller.java
@@ -0,0 +1,15 @@
+package study.querydsl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContoller {
+
+    @GetMapping("/hello")
+    public String hello(String hello) {
+        
+        return "hello" + hello;
+    }
+
+}
