6ad1c0bf3e0aa6146aa5f30a244e895b8e8158f7
aka-nick <bechalrang@gmail.com>
2023-03-28 15:27:29 +0900

    섹션2 - 기본 엔티티 조회 테스트 (케이스 추가)

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -1,11 +1,9 @@
 package study.querydsl.entity;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import javax.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,14 +37,12 @@ class MemberTest {
         em.clear();
 
         // when
-        List<Member> result = em.createQuery("select m from Member m join m.team t ",
-                Member.class).getResultList();
+        List<Member> resultMember = em.createQuery("select m from Member m join m.team t ", Member.class).getResultList();
+        List<Team> resultTeam = em.createQuery("select t from Team t ", Team.class).getResultList();
 
         // then
-        assertThat(result.size()).isEqualTo(4);
-        for (Member member : result) {
-            System.out.println("member = " + member);
-        }
+        assertThat(resultMember.size()).isEqualTo(4);
+        assertThat(resultTeam.size()).isEqualTo(2);
 
     }
 }
