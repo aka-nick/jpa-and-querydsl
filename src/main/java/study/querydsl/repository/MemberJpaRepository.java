b81d95079f81d850b4f80174b8fdc55b99684765
aka-nick <bechalrang@gmail.com>
2023-03-30 16:56:15 +0900

    섹션5 - BooleanBuilder를 통한 동적 쿼리와 성능 최적화 조회

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,21 @@
 package study.querydsl.repository;
 
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.*;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCond;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QTeam;
 
 @Repository
 @RequiredArgsConstructor
@@ -42,4 +49,33 @@ public class MemberJpaRepository {
         return queryFactory.selectFrom(member)
                 .where(member.username.eq(username)).fetch();
     }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCond cond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(cond.getUsername())) {
+            builder.and(member.username.eq(cond.getUsername()));
+        }
+        if (StringUtils.hasText(cond.getTeamName())) {
+            builder.and(team.name.eq(cond.getTeamName()));
+        }
+        if (cond.getAgeGoe() != null) {
+            builder.and(member.age.goe(cond.getAgeGoe()));
+        }
+        if (cond.getAgeLoe() != null) {
+            builder.and(member.age.loe(cond.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder) //모든 조건이 없는 경우, 전체 데이터를 끌어온다. 조건 검색은 페이징 쿼리가 '거의 항상' 필요하다.
+                .fetch();
+    }
+
 }
--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCond;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 @Transactional
 @SpringBootTest
@@ -47,5 +50,33 @@ class MemberJpaRepositoryTest {
 
         List<Member> find2 = memberJpaRepository.findByUsernameQueryDsl("member1");
         assertThat(find2).containsExactly(member1);
+    }
+
+    @Test
+    void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCond cond = new MemberSearchCond();
+        cond.setAgeGoe(35);
+        cond.setAgeLoe(55);
+        cond.setTeamName("teamB");
+        System.out.println("cond = " + cond);
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(cond);
+
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
