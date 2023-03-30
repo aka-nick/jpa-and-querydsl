dd054ef87c887fc2b1aa24bbfc418c01d82ae37c
aka-nick <bechalrang@gmail.com>
2023-03-30 10:24:57 +0900

    섹션4 - 조건문 생성 (BooleanBuilder)

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
@@ -563,4 +564,29 @@ public class QueryDslBasicTest {
         }
 
     }
+
+    @Test
+    void dynamicQueryBooleanBuilder() {
+        String usernameCond = "member1";
+        Integer ageCond = 10;
+
+        List<Member> result = searchMember1(usernameCond, ageCond);
+        assertThat(result.size()).isEqualTo(1);
+    }
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
+
+
 }
