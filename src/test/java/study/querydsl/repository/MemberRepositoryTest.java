de886413b235c168bac4f74776670e86087129b9
aka-nick <bechalrang@gmail.com>
2023-03-31 15:05:52 +0900

    섹션7 - QuerydslPredicateExecutor 기능 사용해보기

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCond;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @Transactional
@@ -104,4 +105,32 @@ class MemberRepositoryTest {
 
     }
 
+
+    @Test
+    void queryDslPredicateExecutorTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> member11 = memberRepository.findAll(
+                member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for (Member o : member11) {
+            System.out.println("o = " + o);
+        }
+
+
+    }
 }
